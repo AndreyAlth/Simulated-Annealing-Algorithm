1110dd21671eb89a07e2a1288e096c41330d3d7c
AndreyAlth <43351614+AndreyAlth@users.noreply.github.com>
2018-11-14 15:26:52 -0600

    Update animacion.py

--- animacion.py
+++ animacion.py
@@ -23,11 +23,24 @@ def obtenerPuntos(archivo):
     :return: La lista de puntos
     '''
 
+    #Se crea una lista vacia
     puntos = []
+    #Abrimos el archivo con la funcion open(nombre, modo)
+    #donde el parametro nombre es el nombre del archivo
+    #el segundo parametro es el modo de abrir el archivo
+    # r significa de read, solo abrimos el archivo para leer
     abrir = open(archivo, "r")
+    #recorremos cada linea del archivo
     for line in abrir:
-        mitad = math.floor((len(line))/2)
-        print(mitad)
+        #la funcion split(), divide la linea del archivo cuando
+        #cuando encuentre el parametro asignado, regresando una lista
+        pareja = line.split(" ")
+        #recorremos los valores de la lista parejas
+        for valor in range(len(pareja)):
+            #convertimos cada valor de string a int
+            pareja[valor] = int(pareja[valor])
+        #insertamos la lista pareja en la lista puntos
+        puntos.append(pareja)
     print(puntos)
     return []
 
