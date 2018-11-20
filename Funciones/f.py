65a031eba2bda0a2c838b057c7484bf5e5680ece
Theo-SABH <45024655+Theo-SABH@users.noreply.github.com>
2018-11-19 23:39:07 -0600

    Update f.py

--- Funciones/f.py
+++ Funciones/f.py
@@ -1,8 +1,14 @@
+#Calcular el valor de la solución actual
+
+archivo = open("puntos.txt", "r")
+puntos = archivo.read()
+
 def f(puntos,x):
-    '''
-    Calcula el valor de la solucion actual
-    :param puntos:
-    :param x:
-    :return:
-    '''
-    return 0.0
+    parejas = x
+    for i in range(parejas):
+        resta_al_cuadrado_a = (puntos[i+1][0] - puntos[i][0]) ** 2
+        resta_al_cuadrado_b = (puntos[i+1][1] - puntos[i][1]) ** 2
+        suma = resta_al_cuadrado_a + resta_al_cuadrado_b
+        distancia = suma**0.5
+        DistanciaT += distancia
+    return DistanciaT
