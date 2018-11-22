02149053477e6bbf15e986bb29ec245f10c7b584
Andrey Alth <43351614+AndreyAlth@users.noreply.github.com>
2018-11-21 18:39:55 -0600

    Add files via upload

---
+++ animacion.py
@@ -0,0 +1,24 @@
+import turtle
+
+def animacion(puntos, soluciones):
+    '''
+    Usar la tortuga para dibujar
+    1) La lista de puntos (en negro)
+    2) La solucion inicial (en gris)
+    3) La solucion final (en verde)
+
+    :param puntos: Lista de puntos a dibujar
+    :param soluciones: Lista de soluciones a modificar
+    :return: None
+    '''
+    return None
+
+
+def obtenerPuntos(archivo):
+    '''
+    Abrir un archivo con lista de puntos x,y uno por linea y regresa
+    una matriz de puntos
+    :param archivo: Nombre del arcbivo
+    :return: La lista de puntos
+    '''
+    return []
