adaa0bc291e009b9cccde938d823e7cf60c6e47e
MR.ROBOT <alucard_soul@outlook.com>
2018-11-22 04:47:10 -0600

    CONTENIDO A ANIMACION Y MAIN

--- animacion.py
+++ animacion.py
@@ -1,6 +1,51 @@
 import turtle
+from turtle import *
+import time
 
 def animacion(puntos, soluciones):
+    """ 1) DIBUJA LOS PUNTOS EN NEGRO"""
+    screensize(800, 800) #screensize define el tamaño de pantalla que se utilizará para la animación
+    t = Turtle() #crea la tortuga
+    t.hideturtle() #esconde la tortuga
+    t.speed(10) #velocidad de la tortuga
+    t.pensize(4)#tamaño de la línea en px
+    t.penup() #levanta el lapiz
+    for p in puntos: #ciclo hasta fin de puntos
+        dx, dy = p #asigna variable a puntos
+        t.goto(dx, dy)  #define las coordenadas de aparicion
+        t.pendown() #escribe
+        t.dot('black') #define el agregar un punto en cada punto marcado además de definir el color
+        t.penup() #levanta el lapiz
+    time.sleep(2) #espera 2s
+
+    """ 2) TRAZA LA SOLUCION INICIAL EN GRIS (agregar primera permutacion)"""
+    t.showturtle() #muestra la tortuga
+    t.speed(4) #velocidad de la tortuga
+    t.pensize(3)#tamaño de la línea en px
+    t.color('gray')#color de la tortuga
+    t.penup() #levanta el lapiz
+    for p in puntos: #ciclo hasta fin de puntos
+        dx, dy = p #asigna variable a puntos
+        t.goto(dx, dy)  #define la dirección que tomará la línea
+        t.pendown() #escribe
+    t.penup() #levanta el lapiz
+    time.sleep(10) #espera 10s
+
+    """ 3) TRAZA LA SOLUCION FINAL EN VERDE
+    pix, piy = 0 #sin asignar nada por el momento
+    t.goto(pix, piy) #punto incial
+    t.speed(4) #velocidad de la tortuga
+    t.pensize(2)#tamaño de la línea en px
+    t.color('green')#color de la tortuga
+    t.penup() #levanta el lapiz
+    for p in soluciones: #ciclo hasta fin de puntos
+        dx, dy = p #asigna variable a puntos
+        t.goto(dx, dy)  #define la dirección que tomará la línea
+        t.pendown() #escribe
+    t.penup() #levanta el lapiz
+    time.sleep(10) #espera 10s
+    """
+
     '''
     Usar la tortuga para dibujar
     1) La lista de puntos (en negro)
@@ -36,4 +81,4 @@ def obtenerPuntos(archivo):
       indice+=1   #incremente el indice de la matriz
     return puntos
 
-#print(obtenerPuntos("puntos.txt"))
+#ejem print(obtenerPuntos("puntos.txt"))
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-
-
+"""
 from sa import *
-
+"""
 from animacion import *
 
-puntos = obtenerPuntos('lista.txt')
+puntos = obtenerPuntos('puntos.txt')
+
+"""
 soluciones = enfriamiento_simulado(puntos, 10000, 10, 0.1, len(puntos))
 animacion(puntos, soluciones)
+"""
+
+animacion(puntos, 0)
