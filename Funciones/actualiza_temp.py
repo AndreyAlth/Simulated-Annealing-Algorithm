96530833639693e807f7ab7f9dcf58510826fecb
MR.ROBOT <alucard_soul@outlook.com>
2018-11-26 21:18:16 -0600

    prueba 2 de branches

--- Funciones/actualiza_temp.py
+++
@@ -1,9 +0,0 @@
-def actualiza_temp(t_ini,t_fin, i):
-    '''
-    Actualiza la temperatura
-    :param t_ini: Temp inicial
-    :param t_fin: Temp final
-    :param i: iteracion actual
-    :return: Nueva temperatura
-    '''
-    return 0.0
--- Funciones/modifica.py
+++
@@ -1,8 +0,0 @@
-def modifica(x,n):
-    '''
-    Modifica la solucion actual haciendo un swap
-    :param x:
-    :param n:
-    :return:
-    '''
-    return x
